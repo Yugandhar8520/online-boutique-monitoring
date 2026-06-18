1d572187dcfc63c8fe53de349c43f40d5ff1a300
Yugandhar8520 <yugandharreddy0258@gmail.com>
2026-06-18 12:50:59 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,8 @@ resource "aws_instance" "example" {
   subnet_id     = var.subnet_id
   key_name      = var.key_name
 
+  user_data = file("${path.module}/install.sh")
+
   tags = {
     Name        = "terraform-ec2"
     Environment = "dev"
